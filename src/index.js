699251ef2a427487f1712492a0a16bca12d702b2
Marcin Szuster <szust.marcin@gmail.com>
2021-01-25 22:04:53 +0100

    Add a function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 require("./initDatabse");
+const Bed = require("./models/bed");
+
+new Bed({bed_number: "eee"}).save().then(() => console.log("eee"))
--- src/models/accommodation.js
+++ src/models/accommodation.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const accommodationSchema = new mongoose.Schema({
-  start_date: Date,
+  start_date: {
+    type: Date,
+    required: function() {
+      return this.start_date < this.end_date;
+    }
+  },
   end_date: Date,
   bed: { type: mongoose.Schema.Types.ObjectId, ref: 'Bed' }
 });
